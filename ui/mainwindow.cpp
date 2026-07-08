202a19b11bfb2ee265927798999a8a56d4921688
MichalNalewajko1 <michaln130@gmail.com>
2026-07-08 17:14:18 +0200

    uruchomienie

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    if(!m_dbManager.initDatabase()){
+        QMessageBox::critical(this, "Błąd krytyczny", "Nie udało się zainicjalizować bazy danych!");
+    }
 }
 
 MainWindow::~MainWindow()
--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../database/databasemanager.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    DatabaseManager m_dbManager;
 };
 #endif // MAINWINDOW_H
